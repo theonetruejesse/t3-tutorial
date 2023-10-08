d1123b5558bd1753000f434261bda77373f7d64f
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2023-10-08 17:21:26 -0500

    styling for posts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,43 @@
-import { z } from "zod";
-
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    imageUrl: user.imageUrl,
+  };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id == post.authorId);
+      if (!author || !author.username)
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Author for post not found",
+        });
+      return {
+        post,
+        // needed since typescript auto-checking is stupid
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
