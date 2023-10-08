36d5e05e9216a3c76e0bd7222477b2a67fcb8d20
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2023-10-08 14:19:35 -0500

    implemented clerk authentication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
-import { api } from "~/utils/api";
+import { api } from "../utils/api";
 
-export default function Home() {
+const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
@@ -45,8 +46,13 @@ export default function Home() {
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
+          <Link href="/protected" className="text-xl text-white">
+            Check out a protected procedure
+          </Link>
         </div>
       </main>
     </>
   );
-}
+};
+
+export default Home;
